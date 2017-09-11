f645b722d31e773b4893c6cb54696f7ac4a4cde4
jaracil <pepe@diselpro.com>
2017-09-11 08:26:12 +0200

    Replace connection type methods by connection type constants

--- plugins/netinfo/netinfo.go
+++ plugins/netinfo/netinfo.go
@@ -4,41 +4,24 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+const (
+	NoneType     = "NONE"
+	EthernetType = "ETHERNET"
+	WifiType     = "WIFI"
+	CellType     = "CELLULAR"
+	Cell2gType   = "2G"
+	Cell3gType   = "3G"
+	Cell4gType   = "4G"
+	Cell5gType   = "5G"
+)
+
 func Type() string {
 	return js.Global.Get("navigator").Get("connection").Get("type").String()
 }
 
-func WifiType() string {
-	return js.Global.Get("Connection").Get("WIFI").String()
-}
-
-func EthernetType() string {
-	return js.Global.Get("Connection").Get("ETHERNET").String()
-}
-
-func Cell2gType() string {
-	return js.Global.Get("Connection").Get("CELL_2G").String()
-}
-
-func Cell3gType() string {
-	return js.Global.Get("Connection").Get("CELL_3G").String()
-}
-
-func Cell4gType() string {
-	return js.Global.Get("Connection").Get("CELL_4G").String()
-}
-
-func CellType() string {
-	return js.Global.Get("Connection").Get("CELL").String()
-}
-
-func NoneType() string {
-	return js.Global.Get("Connection").Get("NONE").String()
-}
-
 func IsCell() bool {
 	t := Type()
-	return t == Cell2gType() || t == Cell3gType() || t == Cell4gType() || t == CellType()
+	return t == Cell2gType || t == Cell3gType || t == Cell4gType || t == Cell5gType || t == CellType
 }
 
 func OnOnline(fn func()) {
