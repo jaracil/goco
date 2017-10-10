9457aedafa68b93951a31f18ff2fbcd794d8ecbe
jaracil <pepe@diselpro.com>
2017-10-10 19:21:33 +0200

    Add support to browser

--- plugins/device/device.go
+++ plugins/device/device.go
@@ -1,6 +1,9 @@
 package device
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/jaracil/goco/plugins/cordova"
 )
@@ -27,6 +30,24 @@ func init() {
 	cordova.OnDeviceReady(func() {
 		DevInfo = &DeviceInfo{
 			Object: js.Global.Get("device"),
+		}
+		if DevInfo.Platform == "browser" {
+			print("In browser platform")
+			DevInfo.Manufacturer = js.Global.Get("navigator").Get("vendor").String()
+			if DevInfo.Manufacturer == "" {
+				DevInfo.Manufacturer = "Mozilla"
+			}
+			res := js.Global.Get("localStorage").Get("cordova_browser_uuid")
+			if res == js.Undefined {
+				uuid := make([]byte, 16)
+				rand.Read(uuid)
+				DevInfo.UUID = hex.EncodeToString(uuid)
+				js.Global.Get("localStorage").Call("setItem", "cordova_browser_uuid", DevInfo.UUID)
+				print("Generating new UUID for browser platform: " + DevInfo.UUID)
+			} else {
+				DevInfo.UUID = res.String()
+				print("browser UUID: " + DevInfo.UUID)
+			}
 		}
 	})
 }
