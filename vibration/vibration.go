3ad3ea32cba138f52455e2096e921185859e74c9
jaracil <pepe@diselpro.com>
2017-10-18 10:38:45 +0200

    Add doc strings to vibration package

--- vibration/vibration.go
+++ vibration/vibration.go
@@ -1,9 +1,17 @@
+// Package vibration is a GopherJS wrapper for cordova vibration plugin.
+//
+// Install plugin:
+//  cordova plugin add cordova-plugin-vibration
 package vibration
 
 import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+// Vibrate has different functionalities based on parameters passed to it.
+// For example:
+//  Vibrate(3000) // vibrate for 3 seconds
+//  Vibrate(3000, 1000, 3000) // vibrate for 3 seconds, wait for 1 second, vibrate for 3 seconds
 func Vibrate(pat ...interface{}) {
 	js.Global.Get("navigator").Call("vibrate", pat)
 }
