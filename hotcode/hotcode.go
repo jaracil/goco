aecd983014673a6752dcd5ec13ea5829db140eaa
Angel Jimenez <ajimenez@nayarsystems.com>
2017-11-09 17:49:52 +0100

    Library for cordova-hot-code-push

---
+++ hotcode/hotcode.go
@@ -0,0 +1,80 @@
+// Package hotcode is a GopherJS wrapper for cordova cordova-hot-code-push.
+//
+// Install plugin:
+//  cordova plugin add cordova-hot-code-push
+//
+// (Noncomplete implementation)
+package hotcode
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/jaracil/goco"
+)
+
+// Options used for FetchUpdate
+type Options struct {
+	*js.Object
+	ConfigFile     string            `js:"configFile"`
+	RequestHeaders map[string]string `js:"requestHeaders"`
+}
+
+var mo *js.Object
+
+func init() {
+	goco.OnDeviceReady(func() {
+		mo = js.Global.Get("chcp")
+	})
+}
+
+// NewOptions creates options object for FetchUpdate
+func NewOptions() *Options {
+	return &Options{Object: js.Global.Get("Object").New()}
+}
+
+// FetchUpdate downloads an update
+func FetchUpdate(opt *Options) (data interface{}, err error) {
+	if mo == nil {
+		return nil, errors.New("Cannot find chcp object")
+	}
+
+	m := map[string]interface{}{}
+
+	if opt.Get("configFile") != nil {
+		m["config-file"] = opt.Get("configFile")
+	}
+
+	if opt.Get("requestHeaders") != nil {
+		m["request-headers"] = opt.Get("requestHeaders")
+	}
+
+	ch := make(chan struct{})
+	mo.Call("fetchUpdate", func(cbErr, cbData *js.Object) {
+		if cbErr != nil {
+			err = fmt.Errorf("Error on fetchUpdate: code=%v, description=%v", cbErr.Get("code"), cbErr.Get("description"))
+		}
+		data = cbData
+		close(ch)
+	}, m)
+	<-ch
+	return data, err
+}
+
+// InstallUpdate installs downloaded update
+func InstallUpdate() (err error) {
+	if mo == nil {
+		return errors.New("Cannot find chcp object")
+	}
+
+	ch := make(chan struct{})
+	mo.Call("installUpdate", func(cbErr *js.Object) {
+		if cbErr != nil {
+			err = fmt.Errorf("Error on installUpdate: code=%v, description=%v", cbErr.Get("code"), cbErr.Get("description"))
+		}
+		close(ch)
+	})
+	<-ch
+	return err
+}
