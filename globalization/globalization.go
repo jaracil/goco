f5d3e3b556050a417063b969cec44a482e2fce51
Angel Jimenez <ajimenez@nayarsystems.com>
2018-10-29 12:31:23 +0100

    Added getDatePattern on globalization package

--- globalization/globalization.go
+++ globalization/globalization.go
@@ -12,6 +12,15 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+// DatePatternInfo is info returned on GetDatePattern
+type DatePatternInfo struct {
+	Pattern      string
+	Timezone     string
+	IANATimezone string
+	UTCOffset    int
+	DSTOffset    int
+}
+
 var instance *js.Object
 
 func mo() *js.Object {
@@ -40,4 +49,31 @@ func GetPreferredLanguage() (lang string, err error) {
 	<-ch
 
 	return lang, err
+}
+
+// GetDatePattern returns info about date formating
+func GetDatePattern() (info *DatePatternInfo, err error) {
+	if mo() == nil {
+		return nil, errors.New("Cannot find navigator.globalization object")
+	}
+
+	ch := make(chan struct{})
+	mo().Call("getDatePattern",
+		func(jsInfo *js.Object) {
+			info = &DatePatternInfo{
+				Pattern:      jsInfo.Get("pattern").String(),
+				Timezone:     jsInfo.Get("timezone").String(),
+				IANATimezone: jsInfo.Get("iana_timezone").String(),
+				UTCOffset:    jsInfo.Get("utc_offset").Int(),
+				DSTOffset:    jsInfo.Get("dst_offset").Int(),
+			}
+			close(ch)
+		},
+		func() {
+			err = errors.New("Error getting date pattern")
+			close(ch)
+		})
+	<-ch
+
+	return info, err
 }
